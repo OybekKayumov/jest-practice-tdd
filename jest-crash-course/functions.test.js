0a207509e6d244365d153582e2c79145bd8f2f52
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-29 20:46:01 +0500

    add object and toEqual (array)

--- jest-crash-course/functions.test.js
+++ jest-crash-course/functions.test.js
@@ -1,30 +1,38 @@
 const functions = require('./functions');
 
+// toBe
 test('adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4)
-})
+});
 
+// not
 test('adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5)
-})
+});
 
 //CHECK FOR TRUTHY & FALSY VALUES
 // toBeNull matches only null
 test('should be null', () => {
   expect(functions.isNull()).toBeNull()
-})
+});
 
 // falsy null
 test('should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy()
-})
+});
 
 // falsy zero
 test('should be falsy', () => {
   expect(functions.checkValue(0)).toBeFalsy()
-})
+});
 
 // falsy undefined
 test('should be falsy', () => {
   expect(functions.checkValue(undefined)).toBeFalsy()
-})
+});
+
+// use toEqual to test objects or an arrays, not use toBe
+test('user should be Bob Bilan object', () => {
+  // expect(functions.createUser()).toBe({ firstName: 'Bob', lastName: "Bilan"});
+  expect(functions.createUser()).toEqual({ firstName: 'Bob', lastName: "Bilan"});
+});
