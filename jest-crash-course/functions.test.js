6601d768253cada602b225a39f51d0b8a2e26cd8
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-29 20:57:33 +0500

    add regex

--- jest-crash-course/functions.test.js
+++ jest-crash-course/functions.test.js
@@ -59,3 +59,23 @@ test('should be under 1600', () => {
   expect(load1 + load2).toBeLessThan(1600);
 });
 
+// toBeLessThanOrEqual
+test('should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+
+//todo Regex
+
+test('there is bo I in team', () => {
+  expect('team').not.toMatch(/I/);  //* teamI will fail
+});
+
+// key sensitive
+test('there is bo I in team', () => {
+  expect('team').not.toMatch(/I/i); //* teamIi will fail
+});
+
+//todo Regex
