933a408290eacf5b24565bba77a63431f6003e44
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-29 20:51:05 +0500

    add toBeLessThan

--- jest-crash-course/functions.test.js
+++ jest-crash-course/functions.test.js
@@ -1,38 +1,61 @@
 const functions = require('./functions');
 
-// toBe
+//todo toBe
+
 test('adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4)
 });
 
-// not
+
+//todo not
+
 test('adds 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5)
 });
 
-//CHECK FOR TRUTHY & FALSY VALUES
-// toBeNull matches only null
+//? CHECK FOR TRUTHY & FALSY VALUES
+//todo toBeNull matches only null
+
 test('should be null', () => {
   expect(functions.isNull()).toBeNull()
 });
 
-// falsy null
+
+//todo falsy null
+
 test('should be falsy', () => {
   expect(functions.checkValue(null)).toBeFalsy()
 });
 
-// falsy zero
+
+//todo falsy zero
+
 test('should be falsy', () => {
   expect(functions.checkValue(0)).toBeFalsy()
 });
 
-// falsy undefined
+
+//todo falsy undefined
+
 test('should be falsy', () => {
   expect(functions.checkValue(undefined)).toBeFalsy()
 });
 
-// use toEqual to test objects or an arrays, not use toBe
+
+//todo use toEqual to test objects or an arrays, not use toBe
+
 test('user should be Bob Bilan object', () => {
-  // expect(functions.createUser()).toBe({ firstName: 'Bob', lastName: "Bilan"});
+  //! error:
+// expect(functions.createUser()).toBe({ firstName: 'Bob', lastName: "Bilan"});
   expect(functions.createUser()).toEqual({ firstName: 'Bob', lastName: "Bilan"});
 });
+
+
+//todo less than and greater than
+
+test('should be under 1600', () => {
+  const load1 = 800;
+  const load2 = 700;
+  expect(load1 + load2).toBeLessThan(1600);
+});
+
