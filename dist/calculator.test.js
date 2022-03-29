59946eae1837d58b73a3784605198b689e60a56f
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-03-29 14:11:33 +0500

    add calculator subtract()

--- dist/calculator.test.js
+++ dist/calculator.test.js
@@ -16,5 +16,18 @@ describe('add()', () => {
   })
 })
 
+describe('subtract()', () => {
+  test('given 2 numbers', () => {
+    expect(calculator.subtract(2, 8)).toBe(-6)
+  })
+
+  test('given 2 numbers', () => {
+    expect(calculator.subtract(-1, -2)).toBe(1)
+  })
+
+  test('given 2 numbers', () => {
+    expect(calculator.subtract(4, -5)).toBe(9)
+  })
+})
 
 
